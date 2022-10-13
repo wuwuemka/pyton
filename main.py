6e356155cdfa51cd61b479b9d75230e0a5f67810
emkacz <78071109+wuwuemka@users.noreply.github.com>
2022-10-13 09:53:46 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+def xxx1(a,b):
+	print(a+b)
+
+def xxx2(a,b):
+	return a+b
+
+def xxx3():
+	a = float(input())
+	b = float(input())
+	return a + b
+
+def xxx4():
+	a = float(input())
+	b = float(input())
+	print (a + b)
+
+print(23.323 % 1== 0)
+print(22 % 1== 0)
+
+inp = float(input())
+if inp % 1== 0 and inp % 2== 0:
+	print("tak")
+else:
+	print("nie")
